50ebaf40ad4485cbd7c6176bbc6039b411899235
WIDANET <willianlps99@outlook.com>
2024-01-08 22:23:27 -0300

    first commit

---
+++ Models/Smartphone.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio_POO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        protected string Modelo { get; set; }
+        protected string Imei { get; set; }
+        protected int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigação()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(String nomeApp);
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using Desafio_POO.Models;
+
+Console.WriteLine("Smartphone: Nokia ");
+Smartphone nokia = new Nokia(numero: "12456", modelo: "X2-00", imei: "11111", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "iPhone 12", imei: "22222", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Fortnite");
